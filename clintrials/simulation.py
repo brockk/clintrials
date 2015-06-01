66d1d9c05f09feb2f724a282dcfdb2a2696a5d74
Kristian Brock <kristian.brock@gmail.com>
2015-06-01 11:13:11 +0100

    Added general simulation looper

--- clintrials/simulation.py
+++ clintrials/simulation.py
@@ -7,6 +7,42 @@ import itertools
 import json
 
 
+def run_sims(sim_func, n1=1, n2=1, out_file=None, **kwargs):
+    """ Run simulations using a delegate function.
+
+    :param sim_func: Delegate function to be called to yield single simulation.
+    :type sim_func: func
+    :param n1: Number of batches
+    :type n1: int
+    :param n2: Number of iterations per batch
+    :type n2: int
+    :param out_file: Location of file for incremental saving after completion of each batch.
+    :type out_file: str
+    :param kwargs: key-word args for sim_func
+    :type kwargs: dict
+
+    .. note::
+
+        - n1 * n2 simualtions are performed, in all.
+        - sim_func is expected to return a JSON-able object
+        - file is saved after each of n1 iterations, where applicable.
+
+    """
+
+    sims = []
+    for j in range(n1):
+        sims1 = [sim_func(**kwargs) for i in range(n2)]
+        sims += sims1
+        if out_file:
+            try:
+                with open(out_file, 'w') as outfile:
+                    json.dump(sims, outfile)
+            except Exception as e:
+                print 'Error writing:', e
+        print j, datetime.now(), len(sims)
+    return sims
+
+
 def sim_parameter_space(sim_func, ps, n1=1, n2=None, out_file=None):
     """ Run simulations using a function and a ParameterSpace.
 

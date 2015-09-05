7e5d1e84fc4fd9f5912c00e6182d6085c204e3a5
Kristian <kristian.brock@gmail.com>
2015-09-05 20:44:38 +0100

    Prep for 0.1 release
    Moved phase2.py and added lots of detail to README

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'clintrials',
+  packages = ['clintrials'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Kristian Brock',
+  author_email = 'kristian.brock@gmail.com',
+  url = 'https://github.com/brockk/clintrials', # use the URL to the github repo
+  download_url = 'https://github.com/brockk/clintrials/tarball/0.1', # I'll explain this in a second
+  keywords = ['clinical', 'trials', 'biostatistics', 'medical', 'statistics'], # arbitrary keywords
+  classifiers = [],
+)
